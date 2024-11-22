c1a68b3cf6ab017368e73c7e223fc673c63d36d2
raschill <rebeccaaschilling@gmail.com>
2024-11-21 21:29:29 -0600

    worked on Sign Up and Login

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,19 +1,22 @@
 import { useState, FormEvent, ChangeEvent } from "react";
 
-import Auth from '../utils/auth';
+import Auth from "../utils/auth";
 import { login } from "../api/authAPI";
+import type { UserLogin } from "../interfaces/UserLogin";
 
 const Login = () => {
-  const [loginData, setLoginData] = useState({
-    username: '',
-    password: ''
+  const [loginData, setLoginData] = useState<UserLogin>({
+    username: "",
+    password: "",
   });
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
     const { name, value } = e.target;
     setLoginData({
       ...loginData,
-      [name]: value
+      [name]: value,
     });
   };
 
@@ -23,33 +26,44 @@ const Login = () => {
       const data = await login(loginData);
       Auth.login(data.token);
     } catch (err) {
-      console.error('Failed to login', err);
+      console.error("Failed to login", err);
     }
   };
 
   return (
-    <div className='container'>
-      <form className='form' onSubmit={handleSubmit}>
+    <div className="form-container">
+      <form className="form login-form" onSubmit={handleSubmit}>
         <h1>Login</h1>
-        <label >Username</label>
-        <input 
-          type='text'
-          name='username'
-          value={loginData.username || ''}
-          onChange={handleChange}
-        />
-      <label>Password</label>
-        <input 
-          type='password'
-          name='password'
-          value={loginData.password || ''}
-          onChange={handleChange}
-        />
-        <button type='submit'>Submit Form</button>
+        <div className="form-group">
+          <label>Username</label>
+          <input
+            placeholder="Username"
+            className="form-input"
+            type="text"
+            name="username"
+            value={loginData.username || ""}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            placeholder="Password"
+            className="form-input"
+            type="password"
+            name="password"
+            value={loginData.password || ""}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <button className="btn btn-primary" type="submit">
+            Login
+          </button>
+        </div>
       </form>
     </div>
-    
-  )
+  );
 };
 
 export default Login;
