863553a84564ed2fccee2d2d8dcbcef413bd3fec
Angelica Mora <angiethelibrarian@gmail.com>
2024-11-26 19:27:54 -0500

    added project acceptance attributes

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -12,61 +12,68 @@ interface ProjectAttributes {
   title: string;
   description?: string;
   budget: number;
-  projectId: number;
+  postedId: number;
   status: 'Open' | 'In Progress' | 'Closed';
   createdDate?: Date;
   updatedDate?: Date;
 }
 
-
-
-interface UserCreationAttributes extends Optional<UserAttributes, 'id'> {}
-
-export class User extends Model<UserAttributes, UserCreationAttributes> implements UserAttributes {
-  public id!: number;
-  public username!: string;
-  public password!: string;
-
-  public readonly createdAt!: Date;
-  public readonly updatedAt!: Date;
-
-  // Hash the password before saving the user
-  public async setPassword(password: string) {
-    const saltRounds = 10;
-    this.password = await bcrypt.hash(password, saltRounds);
-  }
+interface ProjectAcceptanceAttributes {
+  id: number;
+  projectId: number;
+  acceptedId?: number;
+  acceptedDate?: Date;
+  projectStatus: 'Pending' | 'Completed' | 'Suspended';
 }
 
-export function UserFactory(sequelize: Sequelize): typeof User {
-  User.init(
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-      },
-      username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-    },
-    {
-      tableName: 'users',
-      sequelize,
-      hooks: {
-        beforeCreate: async (user: User) => {
-          await user.setPassword(user.password);
-        },
-        beforeUpdate: async (user: User) => {
-          await user.setPassword(user.password);
-        },
-      }
-    }
-  );
 
-  return User;
-}
+// interface UserCreationAttributes extends Optional<UserAttributes, 'id'> {}
+
+// export class User extends Model<UserAttributes, UserCreationAttributes> implements UserAttributes {
+//   public id!: number;
+//   public username!: string;
+//   public password!: string;
+
+//   public readonly createdAt!: Date;
+//   public readonly updatedAt!: Date;
+
+//   // Hash the password before saving the user
+//   public async setPassword(password: string) {
+//     const saltRounds = 10;
+//     this.password = await bcrypt.hash(password, saltRounds);
+//   }
+// }
+
+// export function UserFactory(sequelize: Sequelize): typeof User {
+//   User.init(
+//     {
+//       id: {
+//         type: DataTypes.INTEGER,
+//         autoIncrement: true,
+//         primaryKey: true,
+//       },
+//       username: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//       },
+//       password: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//       },
+//     },
+//     {
+//       tableName: 'users',
+//       sequelize,
+//       hooks: {
+//         beforeCreate: async (user: User) => {
+//           await user.setPassword(user.password);
+//         },
+//         beforeUpdate: async (user: User) => {
+//           await user.setPassword(user.password);
+//         },
+//       }
+//     }
+//   );
+
+//   return User;
+// }
